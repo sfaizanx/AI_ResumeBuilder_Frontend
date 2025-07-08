4ee0eadf15f14d9f5cd8c2cf2315eaf0f1272f02
sayyed faizan <sayyedfaizan1777@outlook.com>
2025-07-08 21:25:03 +0530

    updated +

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://aismartresumebuilder.vercel.app'
+export const BASE_URL = 'https://airesumebuilderbackend-production.up.railway.app'
 //export const BASE_URL = 'http://localhost:3000'
