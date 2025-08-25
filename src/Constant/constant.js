692794a8fd460a22f75f644ea1460b5186db7573
sfaizanx <sayyedfaizan1777@gmail.com>
2025-08-25 18:38:12 +0530

    Url +

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://airesumebuilderbackend-production.up.railway.app'
+export const BASE_URL = 'https://ai-resume-builder-backend-flame.vercel.app'
 //export const BASE_URL = 'http://localhost:3000'
