9039ce68f565d3fb216779495933127ca5a5f3bc
Sayyed Faizan <137804120+sfaizanx@users.noreply.github.com>
2025-10-18 19:29:38 +0530

    Update LandingPage .jsx

--- src/pages/LandingPage .jsx
+++ src/pages/LandingPage .jsx
@@ -229,7 +229,11 @@ const LandingPage = ({ setTokenId }) => {
             {/* Templates Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {templatesToShow?.map((template) => (
-                <Card sx={{ maxWidth: 340 }} key={template.id} onClick={() => navigate(`/aibuilder/${template.id}`)}>
+                <Card sx={{ maxWidth: 340 }} key={template.id} onClick={() => {
+    if (window.innerWidth <= 768) { // only trigger on small screens
+      navigate(`/aibuilder/${template.id}`);
+    }
+  }}>
                   <div className="relative overflow-hidden">
                     {/* Template Preview */}
                     <div className="cursor-pointer md:transform scale-[0.36] origin-top-left w-227 h-75 mx-auto pt-4">
